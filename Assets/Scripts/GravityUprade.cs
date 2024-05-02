bc7d9eac6b139f27b19cc70407658f8c487a4896
VHC-frigid <159389593+VHC-frigid@users.noreply.github.com>
2024-05-02 13:06:50 +1000

    erm what the sigma code

---
+++ Assets/Scripts/GravityUprade.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityUprade : BaseUpgrade
+{
+    protected PlaneController planeController;
+    private float upgradeGravity;
+
+    public GravityUprade(PlaneController plane, int upgradeCost, float newGravity)
+    {
+        planeController = plane;
+        cost = upgradeCost;
+        upgradeGravity = newGravity;
+    }
+
+    public override void ApplyUpgrade()
+    {
+        planeController.gravity -= upgradeGravity;
+        planeController.gravity = Mathf.Clamp(planeController.gravity, 0f, 20f);
+    }
+
+    public override string UpgradeName()
+    {
+        return "Gravity" + upgradeGravity;
+    }
+
+}
--- Assets/Scripts/SpeedUpgrade.cs
+++ Assets/Scripts/SpeedUpgrade.cs
@@ -6,15 +6,33 @@ public class SpeedUpgrade : BaseUpgrade
 {
 
     protected PlaneController planeController;
-
+    private float upgradeSpeed;
     public SpeedUpgrade(PlaneController plane)
     {
         planeController = plane;
     }
 
+    public SpeedUpgrade(PlaneController plane, int upgradeCost)
+    {
+        planeController = plane;
+        cost = upgradeCost;
+    }
+
+    public SpeedUpgrade(PlaneController plane, int upgradeCost, float newSpeed)
+    {
+        planeController = plane;
+        cost = upgradeCost;
+        upgradeSpeed = newSpeed;
+    }
 
     public override void ApplyUpgrade()
     {
-        
+        planeController.speed += upgradeSpeed;
     }
+
+    public override string UpgradeName()
+    {
+        return "Speed " + upgradeSpeed;
+    }
+
 }
